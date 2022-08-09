247aec922ddc09569aebf9e1426b6f3aada91461
Joshua Erinosho <joshuaerinosho@gmail.com>
2022-08-09 15:32:11 +0100

    Cleanup

--- android/src/main/kotlin/com/appdev/internet_speed_test_plus/InternetSpeedTestPlugin.kt
+++ android/src/main/kotlin/com/appdev/internet_speed_test_plus/InternetSpeedTestPlugin.kt
@@ -12,6 +12,7 @@ import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
+import io.flutter.plugin.common.PluginRegistry.Registrar
 
 /** InternetSpeedTestPlugin */
 public class InternetSpeedTestPlugin(internal var activity: Activity, internal var methodChannel: MethodChannel, registrar: Registrar) : MethodCallHandler {
